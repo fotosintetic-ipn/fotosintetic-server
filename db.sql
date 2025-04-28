56c4dbc499385976d4d304b4293d610c11eb7bdd
Nightyly <asciialt59@gmail.com>
2025-04-27 19:28:21 -0600

    updated to work with ph values

--- db.sql
+++ db.sql
@@ -1,63 +1,43 @@
--- MariaDB dump 10.19-11.3.2-MariaDB, for Linux (x86_64)
---
--- Host: localhost    Database: oxim
--- ------------------------------------------------------
--- Server version	11.3.2-MariaDB
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         11.7.2-MariaDB - mariadb.org binary distribution
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             12.10.0.7000
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `users`
---
+-- Volcando estructura para tabla polih.data
+CREATE TABLE IF NOT EXISTS `data` (
+  `entry_id` int(11) NOT NULL AUTO_INCREMENT,
+  `device_id` int(11) NOT NULL,
+  `ph` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  PRIMARY KEY (`entry_id`) USING BTREE,
+  KEY `Índice 2` (`device_id`) USING BTREE,
+  CONSTRAINT `FK_data_devices` FOREIGN KEY (`device_id`) REFERENCES `devices` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=299 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users` (
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla polih.devices
+CREATE TABLE IF NOT EXISTS `devices` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `password` varbinary(50) NOT NULL,
+  `password` varchar(50) NOT NULL DEFAULT '',
+  `device_name` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `vital_signs`
---
+-- La exportación de datos fue deseleccionada.
 
-DROP TABLE IF EXISTS `vital_signs`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `vital_signs` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `received` tinytext NOT NULL,
-  `heartrate` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`heartrate`)),
-  `spo2` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`spo2`)),
-  `heartrate_precision` double NOT NULL,
-  `spo2_precision` double NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_vital_signs_users` (`user_id`),
-  CONSTRAINT `FK_vital_signs_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=299 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-06-09 15:26:18
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
--- source/main.cpp
+++ source/main.cpp
@@ -11,10 +11,10 @@
 
 int main(){
     auto config = std::make_shared<sqlpp::mysql::connection_config>();
-    config -> database = "";
-    config -> user = "";
-    config -> password = "";
-    config -> host = "";
+    config -> database = "polih";
+    config -> user = "pez";
+    config -> password = "pez";
+    config -> host = "localhost";
     config -> debug = true;
     auto db = sqlpp::mysql::connection(config);
 
@@ -24,16 +24,8 @@ int main(){
         if(req.body == "") return crow::response(crow::status::BAD_REQUEST);
         auto body = nlohmann::json::parse(req.body);
         if(req.get_header_value("Authorization") == ""
-        || !body.contains("heartRatePrecision")
-        || !body.contains("spo2Precision")
-        || !body.contains("heartRateArray")
-        || !body.contains("spo2Array")
-        || !body.contains("sent")
-        || !body["heartRatePrecision"].is_number()
-        || !body["spo2Precision"].is_number()
-        || !body["heartRateArray"].is_array()
-        || !body["spo2Array"].is_array()
-        || !body["sent"].is_string()){
+        || !body.contains("ph")
+        || !body["ph"].is_array()){
             return crow::response(crow::status::BAD_REQUEST);
         }
 
@@ -41,65 +33,47 @@ int main(){
         std::string decoded_auth = crow::utility::base64decode(auth, auth.size());
         
         uint64_t found = decoded_auth.find(':');
-        std::string username = decoded_auth.substr(0, found);
+        std::string device_name = decoded_auth.substr(0, found);
         std::string password = decoded_auth.substr(found + 1);
         
-        auto u = oxim::Users{};
-        auto rows = db(select(all_of(u)).from(u).where(u.username == username and u.password == password));
+        auto dev = polih::Devices{};
+        auto rows = db(select(all_of(dev)).from(dev).where(dev.deviceName == device_name and dev.password == password));
 
         if(rows.empty())
             return crow::response(crow::status::UNAUTHORIZED);
 
-        int32_t user_id = rows.front().id;
+        int32_t device_id = rows.front().id;
 
-        auto vs = oxim::VitalSigns{};
-        db(insert_into(vs).set(vs.userId = user_id,
-                               vs.heartrate = body["heartRateArray"].dump(),
-                               vs.spo2 = body["spo2Array"].dump(),
-                               vs.heartratePrecision = static_cast<double>(body["heartRatePrecision"]),
-                               vs.spo2_precision = static_cast<double>(body["spo2Precision"]),
-                               vs.received = static_cast<std::string>(body["sent"])));
+        auto data = polih::Data{};
+        db(insert_into(data).set(data.deviceId = device_id,
+                                 data.ph = body["ph"].dump()));
 
         return crow::response(crow::status::OK);
     });
 
-    CROW_ROUTE(app, "/create_account").methods(crow::HTTPMethod::POST)([&db](const crow::request& req){
-        if(req.url_params.get("username") == nullptr
+    CROW_ROUTE(app, "/register_device").methods(crow::HTTPMethod::POST)([&db](const crow::request& req){
+        if(req.url_params.get("device_name") == nullptr
         || req.url_params.get("password") == nullptr){
             return crow::response(crow::status::BAD_REQUEST);
         }
 
-        std::string username = req.url_params.get("username");
+        std::string device_name = req.url_params.get("device_name");
         std::string password = req.url_params.get("password");
 
-        auto u = oxim::Users{};
-        auto rows = db(select(all_of(u)).from(u).where(u.username == username));
+        auto dev = polih::Devices{};
+        auto rows = db(select(all_of(dev)).from(dev).where(dev.deviceName == device_name));
         
         if(!rows.empty())
             return crow::response(crow::status::CONFLICT);
         
-        db(insert_into(u).set(u.username = username,
-                              u.password = password));
+        db(insert_into(dev).set(dev.deviceName = device_name,
+                                dev.password = password));
 
         return crow::response(crow::status::OK);
     });
 
     CROW_ROUTE(app, "/get_records").methods(crow::HTTPMethod::GET)([&db](const crow::request& req){
-        if(req.get_header_value("Authorization") == ""
-        || (req.url_params.get("before") == nullptr && req.url_params.get("after") == nullptr)
-        || (req.url_params.get("before") != nullptr && req.url_params.get("after") != nullptr)){
-            return crow::response(crow::status::BAD_REQUEST);
-        }
-        try{
-            uint32_t aux;
-            if(req.url_params.get("limit") != nullptr)
-                aux = std::stoi(req.url_params.get("limit"));
-            if(req.url_params.get("after") != nullptr)
-                aux = std::stoi(req.url_params.get("after"));
-            else
-                aux = std::stoi(req.url_params.get("before"));
-        }
-        catch(std::invalid_argument& e){
+        if(req.get_header_value("Authorization") == ""){
             return crow::response(crow::status::BAD_REQUEST);
         }
 
@@ -107,11 +81,11 @@ int main(){
         std::string decoded_auth = crow::utility::base64decode(auth, auth.size());
         
         uint64_t found = decoded_auth.find(':');
-        std::string username = decoded_auth.substr(0, found);
+        std::string device_name = decoded_auth.substr(0, found);
         std::string password = decoded_auth.substr(found + 1);
         
-        auto u = oxim::Users{};
-        auto rows = db(select(all_of(u)).from(u).where(u.username == username and u.password == password));
+        auto dev = polih::Devices{};
+        auto rows = db(select(all_of(dev)).from(dev).where(dev.deviceName == device_name and dev.password == password));
 
         if(rows.empty())
             return crow::response(crow::status::UNAUTHORIZED);
@@ -125,68 +99,28 @@ int main(){
             limit = std::stoi(req.url_params.get("limit"));
         limit = std::min(limit, (uint32_t)100);
 
-        auto vs = oxim::VitalSigns{};
+        auto data = polih::Data{};
         nlohmann::json response_body;
 
         try{
-            if(req.url_params.get("before") != nullptr){
-                auto result = db(select(all_of(vs))
-                                .from(vs)
-                                .where(vs.userId == id and vs.id < std::stoi(req.url_params.get("before")))
-                                .limit(limit)
-                                .order_by(vs.id.desc()));
-                int idx = 0;
+            auto result = db(select(all_of(data))
+                            .from(data)
+                            .where(data.deviceId == id)
+                            .limit(limit)
+                            .order_by(data.entryId.asc()));
+            int idx = 0;
 
-                response_body["data"] = nlohmann::json::array();
+            response_body["data"] = nlohmann::json::array();
 
-                for(const auto& i: result){
-                    response_body["data"][idx]["id"] = static_cast<uint32_t>(i.id);
-                    response_body["data"][idx]["received"] = static_cast<std::string>(i.received);
-                    
-                    auto array = nlohmann::json::parse(static_cast<std::string>(i.heartrate));
-                    response_body["data"][idx]["heartrate"] = nlohmann::json::array();
-                    for(int it = 0; it != 10; it++)
-                        response_body["data"][idx]["heartrate"][it] = array[it];
-                    array = nlohmann::json::parse(static_cast<std::string>(i.spo2));
-                    response_body["data"][idx]["spo2"] = nlohmann::json::array();
-                    for(int it = 0; it != 10; it++)
-                        response_body["data"][idx]["spo2"][it] = array[it];
+            for(const auto& i: result){
+                response_body["data"][idx]["entry_id"] = static_cast<uint32_t>(i.entryId);
+                
+                auto array = nlohmann::json::parse(static_cast<std::string>(i.ph));
+                response_body["data"][idx]["ph"] = nlohmann::json::array();
+                for(int it = 0; it != 10; it++)
+                    response_body["data"][idx]["ph"][it] = array[it];
 
-                    response_body["data"][idx]["heartrate_precision"] = static_cast<double>(i.heartratePrecision);
-                    response_body["data"][idx]["spo2_precision"] = static_cast<double>(i.spo2_precision);
-
-                    idx++;
-                }
-                std::reverse(response_body["data"].begin(), response_body["data"].end());
-            }
-            else{
-                auto result = db(select(all_of(vs))
-                                .from(vs)
-                                .where(vs.userId == id and vs.id > std::stoi(req.url_params.get("after")))
-                                .limit(limit)
-                                .order_by(vs.id.asc()));
-                int idx = 0;
-
-                response_body["data"] = nlohmann::json::array();
-
-                for(const auto& i: result){
-                    response_body["data"][idx]["id"] = static_cast<uint32_t>(i.id);
-                    response_body["data"][idx]["received"] = static_cast<std::string>(i.received);
-                    
-                    auto array = nlohmann::json::parse(static_cast<std::string>(i.heartrate));
-                    response_body["data"][idx]["heartrate"] = nlohmann::json::array();
-                    for(int it = 0; it != 10; it++)
-                        response_body["data"][idx]["heartrate"][it] = array[it];
-                    response_body["data"][idx]["spo2"] = nlohmann::json::array();
-                    array = nlohmann::json::parse(static_cast<std::string>(i.spo2));
-                    for(int it = 0; it != 10; it++)
-                        response_body["data"][idx]["spo2"][it] = array[it];
-
-                    response_body["data"][idx]["heartrate_precision"] = static_cast<double>(i.heartratePrecision);
-                    response_body["data"][idx]["spo2_precision"] = static_cast<double>(i.spo2_precision);
-
-                    idx++;
-                }   
+                idx++;
             }
         }
         catch(sqlpp::exception& e){
@@ -201,16 +135,16 @@ int main(){
     });
 
     CROW_ROUTE(app, "/validate_credentials").methods(crow::HTTPMethod::Get)([&db](const crow::request& req){
-        if(req.url_params.get("username") == nullptr
+        if(req.url_params.get("device_name") == nullptr
         || req.url_params.get("password") == nullptr){
             return crow::response(crow::status::BAD_REQUEST);
         }
 
-        std::string username = req.url_params.get("username");
+        std::string device_name = req.url_params.get("device_name");
         std::string password = req.url_params.get("password");
 
-        auto u = oxim::Users{};
-        auto rows = db(select(all_of(u)).from(u).where(u.username == username and u.password == password));
+        auto dev = polih::Devices{};
+        auto rows = db(select(all_of(dev)).from(dev).where(dev.deviceName == device_name and dev.password == password));
         
         if(!rows.empty())
             return crow::response(crow::status::OK);
